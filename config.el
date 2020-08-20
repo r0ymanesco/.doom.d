da51753555d8cf5f44f5374325cb78655cc579cf
tt2114-ipc <r0ymanesco@gmail.com>
2020-08-20 19:23:52 +0100

    initial commit

---
+++ config.el
@@ -0,0 +1,74 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "r0ymanesco"
+      user-mail-address "r0ymanesco@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Dropbox/org")
+(setq org-default-notes-file "~/Dropbox/org/notes.org")
+(setq org-log-done 'time)
+(setq org-agenda-files (list "~/Dropbox/org/notes.org"))
+(after! org-capture
+  (add-to-list 'org-capture-templates
+               '("u" "Quick note" entry
+                 (file+headline "~/Dropbox/org/notes.org" "Quick Notes")
+                 "* %t %?\n%a" :kill-buffer t)
+               )
+  )
+(after! org-capture
+  (add-to-list 'org-capture-templates
+               '("v" "Quick todo" entry
+                 (file+headline "~/Dropbox/org/notes.org" "Quick Notes")
+                 "* TODO %t %?" :kill-buffer t)
+               )
+  )
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+(require 'magit-todos)
+(magit-todos-mode)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
